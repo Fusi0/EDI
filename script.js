edbd08d78e2c0451df8b49439ad1b8840a1502a2
Jakub Fus <79830319+Fusi0@users.noreply.github.com>
2023-12-29 11:20:23 +0100

    upgraded filtering system

--- script.js
+++ script.js
@@ -41,47 +41,72 @@ function displayData(data) {
     dataContainer.appendChild(table);
 }
 
-// added filter for car_brand model and engine type
-function createFilterInputs() {
+function createFilterInputs(data) {
     var table = document.getElementById('myTable');
     var headerRow = table.querySelector('tr');
 
     for (var columnIndex = 0; columnIndex < headerRow.cells.length; columnIndex++) {
         var columnName = headerRow.cells[columnIndex].textContent;
 
-        if (columnName === 'car_brand' || columnName === 'model' || columnName === 'engine_type') {
+        if (columnName === 'car_brand' || columnName === 'model') {
             var input = document.createElement('input');
             input.type = 'text';
             input.placeholder = 'Filter ' + columnName;
-
             input.addEventListener('input', (function (index) {
                 return function () {
-                    filterTable(index, this.value);
+                    filterTable(); 
                 };
             })(columnIndex));
-
             headerRow.cells[columnIndex].appendChild(input);
+        } else if (columnName === 'engine_type') {
+            var select = document.createElement('select');
+            var options = ['all', 'diesel', 'gasoline', 'gaz', 'electric'];
+
+            options.forEach(option => {
+                var optionElement = document.createElement('option');
+                optionElement.value = option.toLowerCase();
+                optionElement.textContent = option;
+                select.appendChild(optionElement);
+            });
+
+            select.addEventListener('change', function () {
+                filterTable();
+            });
+
+            headerRow.cells[columnIndex].appendChild(select);
         }
     }
 }
 
-
-function filterTable(columnIndex, filterValue) {
+function filterTable() {
     var table, rows, i, x;
     table = document.getElementById('myTable');
     rows = table.rows;
 
     for (i = 1; i < rows.length; i++) {
-        
-        if (columnIndex >= 0 && columnIndex < rows[i].cells.length) {
+        var rowVisible = true;
+
+        for (var columnIndex = 0; columnIndex < table.rows[0].cells.length; columnIndex++) {
+            var columnName = table.rows[0].cells[columnIndex].textContent;
+            var input = table.rows[0].cells[columnIndex].querySelector('input');
+            var select = table.rows[0].cells[columnIndex].querySelector('select');
+            var filterValue = input ? input.value.toLowerCase() : (select ? select.value.toLowerCase() : '');
+
             var cellValue = rows[i].cells[columnIndex].textContent || rows[i].cells[columnIndex].innerText;
 
-            if (cellValue.toLowerCase().includes(filterValue.toLowerCase())) {
-                rows[i].style.display = '';
-            } else {
-                rows[i].style.display = 'none';
+            if (columnName === 'engine_type' && filterValue !== 'all' && filterValue !== cellValue.toLowerCase()) {
+                rowVisible = false;
+                break;
             }
+
+            if (columnName !== 'engine_type' && filterValue !== 'all' && !cellValue.toLowerCase().includes(filterValue)) {
+                rowVisible = false;
+                break;
+            }
+
         }
+
+        rows[i].style.display = rowVisible ? '' : 'none';
     }
 }
 
